dae984bb842377963bc7f96c41b9ef55be0380d2
Cameron McBride <cmcbride@mit.edu>
2018-08-15 18:10:43 -0400

    improved error checking

--- analyzeFCS_RS.m
+++ analyzeFCS_RS.m
@@ -2,12 +2,12 @@ addpath utility
 close all
 
 %choose experiment
-[fcsopts, platenames] = fcsoptions('6hrs run1');
+[fcsopts] = fcsoptions('yili');
 
 %read in data
 [cellstruct, datastruct,compflg] = read_all_fcs(fcsopts.datapath);
 %relabel datafields
-cellstruct = renamefields(cellstruct, platenames);
+cellstruct = renamefields(cellstruct, fcsopts.platenames);
 cellnames = fieldnames(cellstruct);
 
 if ~compflg
@@ -20,18 +20,21 @@ if ~compflg
     cellstructcomp = gatecells(cellstructcomp,RGRnames,'fl4h',[10^2.8,10^6]);
     
     %plot
-    plotsubfield(cellstruct,'fl1h','fl3h',1)
-    plotsubfield(cellstructcomp,'fl1h','fl3h',1)
-    plotsubfield(cellstruct,'fl1h','fl4h',2)
-    plotsubfield(cellstructcomp,'fl1h','fl4h',2)
-    plotsubfield(cellstruct,'fl3h','fl4h',3)
-    plotsubfield(cellstructcomp,'fl3h','fl4h',3)
-    histsubfield(cellstruct,'fl1h',4,'log')
-    histsubfield(cellstructcomp,'fl1h',4,'log')
-    histsubfield(cellstruct,'fl3h',5,'log')
-    histsubfield(cellstructcomp,'fl3h',5,'log')
-    histsubfield(cellstruct,'fl4h',6,'log')
-    histsubfield(cellstructcomp,'fl4h',6,'log')
+    if length(fieldnames(cellstruct)) <= 30
+        disp('plotting data...')
+        plotsubfield(cellstruct,'fl1h','fl3h',1)
+        plotsubfield(cellstructcomp,'fl1h','fl3h',1)
+        plotsubfield(cellstruct,'fl1h','fl2h',2)
+        plotsubfield(cellstructcomp,'fl1h','fl4h',2)
+        plotsubfield(cellstruct,'fl3h','fl2h',3)
+        plotsubfield(cellstructcomp,'fl3h','fl2h',3)
+        histsubfield(cellstruct,'fl1h',4,'log')
+        histsubfield(cellstructcomp,'fl1h',4,'log')
+        histsubfield(cellstruct,'fl3h',5,'log')
+        histsubfield(cellstructcomp,'fl3h',5,'log')
+        histsubfield(cellstruct,'fl2h',6,'log')
+        histsubfield(cellstructcomp,'fl2h',6,'log')
+    end
 else
     %copy over
     cellstrcutcomp = cellstruct;
@@ -41,17 +44,23 @@ else
     cellstructcomp = gatecells(cellstructcomp,RGRnames,'fl4h',[10^2.8,10^6]);
     
     %plot
-    plotsubfield(cellstructcomp,'fl1h','fl3h',1)
-    plotsubfield(cellstructcomp,'fl1h','fl4h',2)
-    plotsubfield(cellstructcomp,'fl3h','fl4h',3)
-    histsubfield(cellstructcomp,'fl1h',4,'log')
-    histsubfield(cellstructcomp,'fl3h',5,'log')
-    histsubfield(cellstructcomp,'fl4h',6,'log')
+    if length(fieldnames(cellstruct)) <= 30
+        plotsubfield(cellstructcomp,'fl1h','fl3h',1)
+        plotsubfield(cellstructcomp,'fl1h','fl4h',2)
+        plotsubfield(cellstructcomp,'fl3h','fl4h',3)
+        histsubfield(cellstructcomp,'fl1h',4,'log')
+        histsubfield(cellstructcomp,'fl3h',5,'log')
+        histsubfield(cellstructcomp,'fl4h',6,'log')
+    end
 end
+
+movingavestruct(cellstruct,500,'fsca','time')
+movingavestruct(cellstruct,500,'ssca','time')
+movingavestruct(cellstruct,500,'fl1h','time')
+movingavestruct(cellstruct,500,'fl4h','time')
 
 %find Ghat, Rhat, Yhat for each trial
 hatstruct = calcFPhats(cellstructcomp);
-
 %find J0's for all pairs
 Jstruct = findJ02(hatstruct);
 
