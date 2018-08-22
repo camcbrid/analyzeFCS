eafc792cdd4f3095396fccfbf709416d04f4a513
Cameron McBride <cmcbride@mit.edu>
2018-08-22 17:54:22 -0400

    Add files via upload

--- analyzeFCS_RS.m
+++ analyzeFCS_RS.m
@@ -2,7 +2,7 @@ addpath utility
 close all
 
 %choose experiment
-[fcsopts] = fcsoptions('yili');
+[fcsopts] = fcsoptions('3 hrs run 1');
 
 %read in data
 [cellstruct, datastruct,compflg] = read_all_fcs(fcsopts.datapath);
@@ -13,54 +13,18 @@ cellnames = fieldnames(cellstruct);
 if ~compflg
     %compensate for fluorescence spillover between channels
     refdata = load('fpspilloverfactors.mat');
-    cellstructcomp = compfluor(cellstruct, refdata.fpstruct);
-    
-    %gate cells
-    RGRnames = cellnames(contains(cellnames,{'R1','R2','R3','GR'}));
-    cellstructcomp = gatecells(cellstructcomp,RGRnames,'fl4h',[10^2.8,10^6]);
-    
-    %plot
-    if length(fieldnames(cellstruct)) <= 30
-        disp('plotting data...')
-        plotsubfield(cellstruct,'fl1h','fl3h',1)
-        plotsubfield(cellstructcomp,'fl1h','fl3h',1)
-        plotsubfield(cellstruct,'fl1h','fl2h',2)
-        plotsubfield(cellstructcomp,'fl1h','fl4h',2)
-        plotsubfield(cellstruct,'fl3h','fl2h',3)
-        plotsubfield(cellstructcomp,'fl3h','fl2h',3)
-        histsubfield(cellstruct,'fl1h',4,'log')
-        histsubfield(cellstructcomp,'fl1h',4,'log')
-        histsubfield(cellstruct,'fl3h',5,'log')
-        histsubfield(cellstructcomp,'fl3h',5,'log')
-        histsubfield(cellstruct,'fl2h',6,'log')
-        histsubfield(cellstructcomp,'fl2h',6,'log')
-    end
-else
-    %copy over
-    cellstrcutcomp = cellstruct;
-    
-    %gate cells
-    RGRnames = cellnames(contains(cellnames,{'R1','R2','R3','GR'}));
-    cellstructcomp = gatecells(cellstructcomp,RGRnames,'fl4h',[10^2.8,10^6]);
-    
-    %plot
-    if length(fieldnames(cellstruct)) <= 30
-        plotsubfield(cellstructcomp,'fl1h','fl3h',1)
-        plotsubfield(cellstructcomp,'fl1h','fl4h',2)
-        plotsubfield(cellstructcomp,'fl3h','fl4h',3)
-        histsubfield(cellstructcomp,'fl1h',4,'log')
-        histsubfield(cellstructcomp,'fl3h',5,'log')
-        histsubfield(cellstructcomp,'fl4h',6,'log')
-    end
+    cellstruct = compfluor(cellstruct, refdata.fpstruct);
 end
 
-movingavestruct(cellstruct,500,'fsca','time')
-movingavestruct(cellstruct,500,'ssca','time')
-movingavestruct(cellstruct,500,'fl1h','time')
-movingavestruct(cellstruct,500,'fl4h','time')
+%gate cells
+%gate on singlets
+cellstruct = gatesinglets(cellstruct,'fsca','fsch');
+%gate on low RFP
+RGRnames = cellnames(contains(cellnames,{'R1','R2','R3','GR','RY'}));
+cellstruct = gatecells(cellstruct,RGRnames,'fl4h',[10^2.8,10^6]);
 
 %find Ghat, Rhat, Yhat for each trial
-hatstruct = calcFPhats(cellstructcomp);
+hatstruct = calcFPhats(cellstruct);
 %find J0's for all pairs
 Jstruct = findJ02(hatstruct);
 
@@ -70,3 +34,25 @@ subplot(211);
 barplotstruct(hatstruct);
 subplot(212);
 barplotstruct(Jstruct);
+
+return
+
+%plot
+if length(fieldnames(cellstruct)) <= 6
+    plotsubfield(cellstruct,'fl1h','fl3h',1)
+    plotsubfield(cellstruct,'fl1h','fl2h',2)
+    plotsubfield(cellstruct,'fl3h','fl2h',3)
+    histsubfield(cellstruct,'fl1h',4,'log')
+    histsubfield(cellstruct,'fl3h',5,'log')
+    histsubfield(cellstruct,'fl2h',6,'log')
+end
+
+cellstruct = normalizefields(cellstruct,'fl1h','ssca','time');
+cellstruct = normalizefields(cellstruct,'fl2h','ssca','time');
+cellstruct = normalizefields(cellstruct,'fl3h','ssca','time');
+
+movingavestruct(cellstruct,500,'fsca','time')
+movingavestruct(cellstruct,500,'ssch','time')
+movingavestruct(cellstruct,500,'fl1h','time')
+movingavestruct(cellstruct,500,'fl2h','time')
+movingavestruct(cellstruct,500,'fl3h','time')
