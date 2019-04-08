d99d362cfec8afe23b0c21e8f8a14068926af217
Cameron McBride <cmcbride@mit.edu>
2019-04-08 15:55:24 -0400

    Add files via upload

--- analyzeFCS_RS.m
+++ analyzeFCS_RS.m
@@ -2,7 +2,8 @@ addpath utility
 close all
 
 %choose experiment
-[fcsopts] = fcsoptions('3 hrs run 1');
+%select folder with experimental data inside
+[fcsopts] = fcsoptions('');
 
 %read in data
 [cellstruct, datastruct,compflg] = read_all_fcs(fcsopts.datapath);
@@ -10,21 +11,42 @@ close all
 cellstruct = renamefields(cellstruct, fcsopts.platenames);
 cellnames = fieldnames(cellstruct);
 
-if ~compflg
-    %compensate for fluorescence spillover between channels
-    refdata = load('fpspilloverfactors.mat');
-    cellstruct = compfluor(cellstruct, refdata.fpstruct);
-end
-
-%gate cells
 %gate on singlets
 cellstruct = gatesinglets(cellstruct,'fsca','fsch');
-%gate on low RFP
-RGRnames = cellnames(contains(cellnames,{'R1','R2','R3','GR','RY'}));
-cellstruct = gatecells(cellstruct,RGRnames,'fl4h',[10^2.8,10^6]);
 
+%smooth fields
+% movingavestruct(cellstruct,500,'fl1h','time',2)
+% movingavestruct(cellstruct,500,'fl2h','time',3)
+% movingavestruct(cellstruct,500,'fl3h','time',4)
+
+if ~compflg
+    %compensate for fluorescence spillover between channels
+    %refdata = load('fpspilloverfactors.mat');
+    [cellstructcomp,mstruct,fitstruct] = compfluor2(cellstruct,0);
+end
+
+movingavestruct(cellstructcomp,500,'fl1h','time',2)
+movingavestruct(cellstructcomp,500,'fl2h','time',3)
+movingavestruct(cellstructcomp,500,'fl3h','time',4)
+
+% estimatedist(cellstructcomp,'fl1h','gamma',5);
+% estimatedist(cellstructcomp,'fl2h','gamma',6);
+% estimatedist(cellstructcomp,'fl3h','gamma',7);
+% estimatedist(cellstructcomp,'fl1h','lognormal',5);
+% estimatedist(cellstructcomp,'fl2h','lognormal',6);
+% estimatedist(cellstructcomp,'fl3h','lognormal',7);
+histsubfield(cellstructcomp,'fl1h',5,'log')
+histsubfield(cellstructcomp,'fl2h',6,'log')
+histsubfield(cellstructcomp,'fl3h',7,'log')
+
+%gate cells
+%gate on low RFP
+%RGRnames = cellnames(contains(cellnames,{'R1','R2','R3','GR','RY'}));
+%cellstruct = gatecells(cellstruct,RGRnames,'fl4h',[10^2.8,10^6]);
+
+return
 %find Ghat, Rhat, Yhat for each trial
-hatstruct = calcFPhats(cellstruct);
+hatstruct = calcFPhats(cellstructcomp);
 %find J0's for all pairs
 Jstruct = findJ02(hatstruct);
 
@@ -35,8 +57,7 @@ barplotstruct(hatstruct);
 subplot(212);
 barplotstruct(Jstruct);
 
-return
-
+%old
 %plot
 if length(fieldnames(cellstruct)) <= 6
     plotsubfield(cellstruct,'fl1h','fl3h',1)
